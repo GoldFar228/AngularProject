42dd2798c89e2e79fb1b7fb850bcd3736de0d143
GoldFar <96344369+GoldFar228@users.noreply.github.com>
2024-05-04 12:09:09 +0500

    added authGuard

--- TrainingApp/src/app/services/auth.service.ts
+++ TrainingApp/src/app/services/auth.service.ts
@@ -1,10 +1,34 @@
-import { Injectable, signal } from "@angular/core";
+import { Injectable, Signal, inject, signal } from "@angular/core";
 import { UserInterface } from "../models/user.interface";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
     providedIn: 'root'
 })
 
-export class AuthService{
+export class AuthService {
     currentUserSig = signal<UserInterface | undefined | null>(undefined);
+
+    http = inject(HttpClient)
+
+    auth(): void {
+        this.http.get<{ user: UserInterface }>('https://api.realworld.io/api/user').subscribe({
+            next: (response) => {
+                this.currentUserSig.set(response.user);
+            },
+            error: () => {
+                this.currentUserSig.set(null);
+            }
+        });
+    }
+
+    login(): void {
+        document.location.href = "/";
+    }
+
+    logout(): void {
+        localStorage.setItem('token', '');
+        this.currentUserSig.set(null);
+    }
+
 }
