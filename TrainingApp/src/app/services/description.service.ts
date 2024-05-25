b12f803c30c0c5a540288faa531895cf17151543
easyThingsLover <borderlandzbon@gmail.com>
2024-05-25 12:08:18 +0500

    Update description.service.ts

--- TrainingApp/src/app/services/description.service.ts
+++ TrainingApp/src/app/services/description.service.ts
@@ -4,9 +4,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class DescriptionService {
-  private descriptions: {[key :string] : string} = 
+  private descriptions: Record<string, string> = 
   {"Get stronger": "качайся много ешь куринную грудку", 'Gain weight': 'качайся много ешь гречку и куринную грудку'};
-  getData(): {[key :string] : string} {
+  getData(): Record<string, string> {
     return this.descriptions;
   }
 }
