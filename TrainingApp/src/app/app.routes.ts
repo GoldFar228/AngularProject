f0228870d9484e2491d2de56cd20024393f3762d
easyThingsLover <borderlandzbon@gmail.com>
2024-05-27 12:41:24 +0500

    Update app.routes.ts

--- TrainingApp/src/app/app.routes.ts
+++ TrainingApp/src/app/app.routes.ts
@@ -1,32 +1,33 @@
 import { Routes } from '@angular/router';
 import { TrainingDetailComponent } from './components/trainings/training-detail/training-detail.component';
+import { AuthGuard } from './guards/authGuard.guard';
 
 export const routes: Routes = [
     {path: '', loadComponent: () =>
         import('./components/authentication/authentication.component').then(
             (m) => m.AuthenticationComponent
-        ) },
+        ), canActivate: [AuthGuard] },
     {path: 'home', loadComponent: () =>
         import('./components/home/home.component').then(
             (m) => m.HomeComponent
-        )},
+        ), canActivate: [AuthGuard]},
     {path: 'registration', loadComponent: () =>
         import('./components/authorization/authorization.component').then(
             (m) => m.AuthorizationComponent
-        )},
+        ), canActivate: [AuthGuard]},
     {path: 'trainings', loadComponent: () =>
         import('./components/trainings/trainings.component').then(
             (m) => m.TrainingsComponent
-        )},
+        ), canActivate: [AuthGuard]},
     {path: 'diets', loadComponent: () =>
         import('./components/diets/diets.component').then(
-            (m) => m.DietsComponent)},
+            (m) => m.DietsComponent), canActivate: [AuthGuard]},
     {path: 'profile', loadComponent: () =>
         import('./components/profile/profile.component').then(
-            (m) => m.ProfileComponent)},
+            (m) => m.ProfileComponent), canActivate: [AuthGuard]},
     {path: 'detail/:id', loadComponent: () =>
         import('./components/trainings/training-detail/training-detail.component').then(
-            (m) => m.TrainingDetailComponent)},
+            (m) => m.TrainingDetailComponent), canActivate: [AuthGuard]},
 ];
 
 
