9f98acb3bc3ff742040e545b2bd886295520e6fe
GoldFar <96344369+GoldFar228@users.noreply.github.com>
2024-06-02 23:13:13 +0500

    some style changes and simple corrections

--- TrainingApp/src/app/mock-data/trainings.mock.ts
+++ TrainingApp/src/app/mock-data/trainings.mock.ts
@@ -4,25 +4,34 @@ import { Injectable } from "@angular/core";
 
 @Injectable()
 export class TrainingsMock {
-  public getTrainings(): Observable<Training[]> {
-    return of([{
-      header: "Increase your bench press",
-      img: "assets/man-barbell-bench-press.png",
-      tag1: "Get stronger",
-      tag1Img: "assets/get-stronger.jpg",
-      tag2: "30 minutes",
-      tag2Img: "assets/30-mins.jpg",
-      des: false
-    },
-    {
-      header: "Chest growth in the gym",
-      img: "assets/chest-growth.jpg",
-      tag1: "Gain weight",
-      tag1Img: "assets/gain-weight.jpg",
-      tag2: "30 minutes",
-      tag2Img: "assets/30-mins.jpg",
-      des: false
-    }]
-    )
-  }
+  // public getTrainings(): Observable<Training[]> {
+  //   return of([{
+  //     header: "Increase your bench press",
+  //     img: "assets/man-barbell-bench-press.png",
+  //     tag1: "Get stronger",
+  //     tag1Img: "assets/get-stronger.jpg",
+  //     tag2: "30 minutes",
+  //     tag2Img: "assets/30-mins.jpg",
+  //     des: false
+  //   },
+  //   {
+  //     header: "Chest growth in the gym",
+  //     img: "assets/chest-growth.jpg",
+  //     tag1: "Gain weight",
+  //     tag1Img: "assets/gain-weight.jpg",
+  //     tag2: "30 minutes",
+  //     tag2Img: "assets/30-mins.jpg",
+  //     des: false
+  //   },
+  //   {
+  //     header: "Chest growth in the gym",
+  //     img: "assets/chest-growth.jpg",
+  //     tag1: "Gain weight",
+  //     tag1Img: "assets/gain-weight.jpg",
+  //     tag2: "30 minutes",
+  //     tag2Img: "assets/30-mins.jpg",
+  //     des: false
+  //   }]
+  //   )
+  // }
 }
