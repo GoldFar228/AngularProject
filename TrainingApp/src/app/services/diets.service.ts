d1e44bae4302dcb055dff1053d663d57745790dc
GoldFar <96344369+GoldFar228@users.noreply.github.com>
2024-09-18 20:53:16 +0500

    1

--- TrainingApp/src/app/services/diets.service.ts
+++ TrainingApp/src/app/services/diets.service.ts
@@ -11,7 +11,7 @@ export class DietsService {
     public calculateCalories(weight: string, height: string, age: string, gender: string){
         if(gender === "male"){
           this.loseWeight = +(((+weight * 10 + +height * 6.25
-            - +age * 5 + 5) * 1.2 * 0.8).toFixed(0)); //1.2 множитель - минимальный множитель активности. Чем выше активность, тем больше множитель
+            - +age * 5 + 5) * 1.2 * 0.8).toFixed(0)); 
           this.keepWeight = +(((+weight * 10 + +height * 6.25
             - +age * 5 + 5) * 1.2).toFixed(0));
           this.gainWeight = +(((+weight * 10 + +height * 6.25
